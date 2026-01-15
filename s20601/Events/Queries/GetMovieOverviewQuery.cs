6040023a491620688a3fb16c079b17e453642ae2
vpvrple <javissimo1@gmail.com>
2026-01-15 01:59:25 +0100

    Remove unnecessary classes

--- s20601/Events/Queries/GetMovieOverviewQuery.cs
+++
@@ -1,5 +0,0 @@
-﻿using MediatR;
-
-namespace s20601.Events.Queries;
-
-public record GetMovieOverviewQuery(string ImdbId) : IRequest<string?>;
--- s20601/ExternalServices/ITmdbLibClient.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace s20601.ExternalServices;
-
-public interface ITmdbLibClient
-{
-    Task<string?> GetPosterUrlByImdbIdAsync(string imdbId);
-    Task<string?> GetMovieOverviewByImdbIdAsync(string imdbId);
-}
